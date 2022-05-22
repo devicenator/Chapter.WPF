4ff38279391b32eeecfd5b3f5899aa5c19d57e90
David Wendland <the.devicenator@gmail.com>
2022-05-22 09:22:57 +0200

    Add tryout project

---
+++ Tryout/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿// 
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// 
+
+using System;
+using System.Diagnostics;
+using SniffCore.Windows;
+
+namespace Tryout;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var watcher = new KeyboardWatcher();
+        watcher.AddCallback(Callback);
+        watcher.Begin();
+    }
+
+    private void Callback(KeyStateChangedArgs obj)
+    {
+        Debug.WriteLine(obj.State);
+    }
+}
