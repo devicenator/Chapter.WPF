3ed9cfea170afef66585813640109b6861e66346
David Wendland <the.devicenator@gmail.com>
2022-05-30 21:02:02 +0200

    Merge drop from library SniffCore.DragNDrop

---
+++ SniffCore.Windows/Drop/DropItemCollection.cs
@@ -0,0 +1,32 @@
+﻿// 
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// 
+
+using System.Windows;
+
+// ReSharper disable once CheckNamespace
+
+namespace SniffCore.Windows
+{
+    /// <summary>
+    ///     The drop item collection to attach on a control using the <see cref="Drop.DropItemsProperty" />.
+    /// </summary>
+    /// <example>
+    ///     <code lang="XAML">
+    /// <![CDATA[
+    /// <ListBox AllowDrop="True">
+    ///     <dragNDrop:Drop.DropItems>
+    ///         <dragNDrop:DropItemCollection>
+    ///             <dragNDrop:DropItem DropType="File" DragDropEffect="Copy" Command="{Binding DropFileCommand}" />
+    ///             <dragNDrop:DropItem DropType="Folder" DragDropEffect="Copy" Command="{Binding DropFolderCommand}" />
+    ///         </dragNDrop:DropItemCollection>
+    ///     </dragNDrop:Drop.DropItems>
+    /// </ListBox>
+    /// ]]>
+    /// </code>
+    /// </example>
+    public sealed class DropItemCollection : FreezableCollection<DropItem>
+    {
+    }
+}
---
+++ SniffCore.Windows/Drop/DropType.cs
@@ -0,0 +1,40 @@
+﻿// 
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// 
+
+// ReSharper disable once CheckNamespace
+
+namespace SniffCore.Windows
+{
+    /// <summary>
+    ///     Items allowed to drop within <see cref="DropItem" />.
+    /// </summary>
+    public enum DropType
+    {
+        /// <summary>
+        ///     The user can drop single files.
+        /// </summary>
+        File,
+
+        /// <summary>
+        ///     The user can drop multiple files.
+        /// </summary>
+        Files,
+
+        /// <summary>
+        ///     The user can drop single folders.
+        /// </summary>
+        Folder,
+
+        /// <summary>
+        ///     The user can drop multiple folders.
+        /// </summary>
+        Folders,
+
+        /// <summary>
+        ///     The user can drop multiple files and folders.
+        /// </summary>
+        FilesAndFolders
+    }
+}
